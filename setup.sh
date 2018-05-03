7e657a224441295c31e2c19c76921e2d581a24d8
Yusuke Hosonuma <tobi462@gmail.com>
2018-05-03 19:08:12 +0900

    update: setup.sh

--- setup.sh
+++ setup.sh
@@ -4,5 +4,5 @@ DOT_FILES=( .gitconfig .tigrc .vimrc .zshrc .zprofile)
 
 for file in ${DOT_FILES[@]}
 do
-    ln -s $HOME/dotfiles/$file $HOME/$file
+    ln -s $HOME/.ghq/github.com/YusukeHosonuma/dotfiles/$file $HOME/$file
 done
