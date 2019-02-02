37b45d0df28bd320b0a18eacd23ec10e13b1a81a
Yusuke Hosonuma <hosonumayuusuke@MBP15-Kou.local>
2019-02-03 06:24:34 +0900

    fix: setup.sh

--- setup.sh
+++ setup.sh
@@ -4,5 +4,5 @@ DOT_FILES=( .gitconfig .gitignore .pryrc .tigrc .vimrc .zshrc)
 
 for file in ${DOT_FILES[@]}
 do
-    ln -s $HOME/.ghq/github.com/YusukeHosonuma/dotfiles/$file $HOME/$file
+    ln -s `ghq root`/github.com/YusukeHosonuma/dotfiles/$file $HOME/$file
 done
